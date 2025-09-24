2a6b11ea22400607314fb8f2ace8e8bd3eb56ddb
HossainMaruf <maruficepustian@gmail.com>
2025-09-24 16:23:59 +0600

    basic query works!

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -2,7 +2,9 @@ package com.library;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App {
     public static void main( String[] args ) {
@@ -10,16 +12,35 @@ public class App {
         String url = "jdbc:mysql://localhost:3306/library";  // Change to your DB info
         String username = "root";                               // Your DB username
         String password = "";                       // Your DB password
+        Connection conn = null;
+        Statement stmt = null;
 
-        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+        try{
+            conn = DriverManager.getConnection(url, username, password);
             if (conn != null && !conn.isClosed()) {
                 System.out.println("Connected to the database successfully!");
+                String sql = "select * from users";
+                stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                while(rs.next()) {
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    System.out.println(id + " " + name);
+                }
+                rs.close();
             } else {
                 System.out.println("Failed to make connection!");
             }
         } catch (SQLException e) {
             System.out.println("SQL Exception: " + e.getMessage());
             e.printStackTrace();
+        } finally {
+            try {
+                if(stmt != null) stmt.close();
+                if(conn != null) conn.close();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
