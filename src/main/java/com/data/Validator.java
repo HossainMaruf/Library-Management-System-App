2f3ed134ae5142bf1af278e79e0670cdd691e7df
HossainMaruf <maruficepustian@gmail.com>
2025-09-25 17:37:47 +0600

    Data Validator class code added but not complete

--- src/main/java/com/data/Validator.java
+++ src/main/java/com/data/Validator.java
@@ -1,5 +1,42 @@
 package com.data;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Validator {
-    
+    private String data;
+    private int minLength;
+    private String minLengthError;
+    private int maxLength;
+    private String maxLengthError;
+
+    List<String> errors = null;
+
+    public Validator(String data) { 
+        this.data = data;
+        errors = new ArrayList<String>();
+    }
+    public boolean validateText(String name) {
+
+    }
+    public Validator min(int minLength) {
+        this.minLength = minLength;
+        if(data.length() < minLength) {
+            errors.add("Length must be at least " + minLength + " letters");
+        }
+        return this;
+    }
+    public Validator max(int maxLength) {
+        this.maxLength = maxLength;
+        if(data.length() > maxLength) {
+            errors.add("Length exceed " + maxLength + " letters");
+        }
+        return this;
+    }
+    public boolean hasError() {
+        return !errors.isEmpty();
+    }
+    public List<String> getErrors() {
+        return errors;
+    }
 }
